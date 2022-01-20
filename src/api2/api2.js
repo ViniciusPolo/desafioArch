883cbd9e5bcd397b4b634e59465c97a69a6fa3de
ViniciusPolo <admviniciuspolo@gmail.com>
2022-01-20 14:30:22 -0300

    finalização api2

--- src/api2/api2.js
+++ src/api2/api2.js
@@ -1,17 +1,41 @@
-const {Client} = require('pg')
+const pg = require('pg')
+const express = require('express')
+const app = express()
 const db = require('../config/database')
 
-const client = new Client(db)
+const pool = new pg.Pool({
+    user: "postgres",
+    database: "arch",
+    password: "kamisama123",
+    port: 5432
+})
 
-async function listData(){
-    client.connect()
-
-    client.query('select (na.n1 + na.n2 + na.n3 + na.n4)/4 as soma' 
-    + 'from notas_aluno na'
-    + 'inner join alunos a on na.id_aluno = a.id' ,(err,res)=>{
-        console.log(err ? " não conectado" : res.rows[0])
-        client.end()
+app.get('/boletim', (req,res,next) => {
+    pool.connect(function (err, client, done){
+        if(err){
+            console.log("Não conectado ao BD " + err);
+        }
+        client.query('select a.nome,(na.n1 + na.n2 + na.n3 + na.n4)/4 as media,' 
+        + '\nCASE'
+        + '\nWHEN (na.n1 + na.n2 + na.n3 + na.n4)/4  < 4 THEN 0'
+        + '\nWHEN (na.n1 + na.n2 + na.n3 + na.n4)/4  >= 4 and (na.n1 + na.n2 + na.n3 + na.n4)/4  < 6 then 5'
+        + '\nWHEN (na.n1 + na.n2 + na.n3 + na.n4)/4  >=6 THEN 10'
+        + '\nEND SITUACAO from notas_aluno na' 
+        + '\ninner join alunos a on na.id_aluno = a.id', function(err, result){
+            done();
+            if(err){
+                console.log(err);
+                res.status(400).send(err)
+            }
+            res.status(200).send(result.rows)
+            console.log("Situação:\n0 - Reprovado\n5 - Recuperação\n10 - Aprovado\n Resultado abaixo no bash")
+            console.log(result)
+        
+        })
     })
-}
+})
 
-listData()
+app.listen(3334, function(){
+    console.log('Server is running on port 3334')
+})
+
